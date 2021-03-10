b1f6c654f21a675203d7e6dcc7baa7f8419f5a0d
Atndesign <Atnnetworkk@gmail.com>
2021-03-10 19:17:22 +0100

    Add Comfy JS support

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -1,3 +1,4 @@
+import { use } from "matter";
 import Phaser from "phaser";
 import Moo from "./sprites/Moo.js";
 import cowTiletSheet from "/assets/spritesheet-cow.png";
@@ -9,11 +10,15 @@ export default class PlayScene extends Phaser.Scene {
       physics: {
         arcade: {
           gravity: { y: 300 },
-          debug: true,
+          debug: false,
         },
       },
     });
     this.viewers = [];
+    this.viewersJson = {
+      viewers: [{}],
+    };
+    ComfyJS.Init(this.getUrlVars()["channel"]);
   }
   preload() {
     this.load.spritesheet("cow", cowTiletSheet, {
@@ -23,7 +28,42 @@ export default class PlayScene extends Phaser.Scene {
   }
 
   create() {
-    this.viewers.push(new Moo(this, "Lorem"));
+    for (let index = 0; index < 5; index++) {
+      this.viewers.push(new Moo(this, "userName"));
+    }
+    ComfyJS.onCommand = (user, command, message, flags, extra) => {
+      if (command === "play") {
+        var canSpawn = true;
+        if (this.viewersJson.viewers.length < 1) {
+          canSpawn = true;
+        }
+        this.viewersJson.viewers.forEach((element) => {
+          if (element.name == user) {
+            canSpawn = false;
+          }
+        });
+        if (canSpawn) {
+          this.createViewer(user);
+          this.viewersJson.viewers.push({ name: user });
+          return;
+        }
+      }
+    };
+  }
+
+  getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(
+      /[?&]+([^=&]+)=([^&]*)/gi,
+      function (m, key, value) {
+        vars[key] = value;
+      }
+    );
+    return vars;
+  }
+
+  createViewer(userName) {
+    this.viewers.push(new Moo(this, userName));
   }
 
   update() {
